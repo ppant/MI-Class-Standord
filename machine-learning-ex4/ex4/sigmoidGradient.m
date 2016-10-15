005abf7c52b1c1e44205846422953ca5fa61401d
Pradeep K. Pant <ppant@cpan.org>
2016-10-15 13:47:26 +0530

    Upadating other files and form for submit

---
+++ machine-learning-ex4/ex4/sigmoidGradient.m
@@ -0,0 +1,34 @@
+function g = sigmoidGradient(z)
+%SIGMOIDGRADIENT returns the gradient of the sigmoid function
+%evaluated at z
+%   g = SIGMOIDGRADIENT(z) computes the gradient of the sigmoid function
+%   evaluated at z. This should work regardless if z is a matrix or a
+%   vector. In particular, if z is a vector or matrix, you should return
+%   the gradient for each element.
+
+g = zeros(size(z));
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Compute the gradient of the sigmoid function evaluated at
+%               each value of z (z can be a matrix, vector or scalar).
+
+
+% ppant: This will return the gradient of sigmoid function, z is a matrix
+g = sigmoid(z) .* (1 - sigmoid(z));
+
+
+
+
+
+
+
+
+
+
+
+% =============================================================
+
+
+
+
+end
